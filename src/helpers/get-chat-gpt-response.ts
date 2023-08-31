9335cafa2b4bc67f9c39af9ffe63b7ab1a98ff22
Jhon Fabio Cardona Martinez <fabio.cardona@coytex.com.co>
2023-08-31 16:39:24 -0500

    Ajustando helpers

--- src/helpers/get-chat-gpt-response.ts
+++ src/helpers/get-chat-gpt-response.ts
@@ -1,17 +1,18 @@
 // This code is for v4 of the openai package: npmjs.com/package/openai
-import { OpenAI } from "openai";
+// import { OpenAI } from "openai";
 
-const openai = new OpenAI({
-    // apiKey: process.env.OPENAI_API_KEY,
-    apiKey: import.meta.env.PUBLIC_OPEN_AI_KEY,
-});
+// const openai = new OpenAI({
+//     // apiKey: process.env.OPENAI_API_KEY,
+//     apiKey: import.meta.env.PUBLIC_OPEN_AI_KEY,
+// });
 
-const response = await openai.completions.create({
-    model: "text-davinci-003",
-    prompt: "",
-    temperature: 0.7,
-    max_tokens: 60,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-});
+// const response = await openai.completions.create({
+//     model: "text-davinci-003",
+//     prompt: "",
+//     temperature: 0.7,
+//     max_tokens: 60,
+//     top_p: 1,
+//     frequency_penalty: 0,
+//     presence_penalty: 0,
+// });
+
